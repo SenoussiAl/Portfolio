cf025cd04aad847c68723c344b7184b6e16cae55
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2024-12-24 20:02:59 -0500

    Added Header and Navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
+/**
+ * @copyright 2024 Senoussi Al-Nadjib
+ * @license Apache-2.0
+ */
+
+/**
+ * Node modules
+ */
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
